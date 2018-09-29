70969440656fbf94fe44465da261d9fdc24ed6c9
Eric Whimpers <kryptocrashmh@gmail.com>
2018-09-28 20:06:49 -0400

    Added stuff

--- server.js
+++ server.js
@@ -6,21 +6,33 @@ var io = require('socket.io').listen(server);
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
 });
-app.use('/client',express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/client'));
 
 server.listen(8080);
 var socket_list = {};
 io.sockets.on("connection", (socket) => {
     console.log("connection");
-    socket.id = Math.random();
-    socket.x = 0;
-    socket.y = 0;
-    socket_list[socket.id] = socket;
 
-    socket.on("disconnect", (socket) => {
-        console.log("Disconnect");
+    var players = {};
+        // create a new player and add it to our players object
+            players[socket.id] = {
+                rotation: 0,
+                x: Math.floor(Math.random() * 700) + 50,
+                y: Math.floor(Math.random() * 500) + 50,
+                layerId: socket.id,
+                team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+            };
+        // send the players object to the new player
+            socket.emit('currentPlayers', players);
+        // update all other players of the new player
+            socket.broadcast.emit('newPlayer', players[socket.id]);
+
+        socket.on("disconnect", (socket) => {
+                console.log("Disconnect");
+            // remove this player from our players object
+                delete players[socket.id];
+            // emit a message to all players to remove this player
+                io.emit('disconnect', socket.id);
     });
-    socket.emit("hey", {
-        reason: "whats up",
-    });
+
 });
